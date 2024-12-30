e066576fe9cf8daea0dd932d6fb3da991043c88e
Kevin Großmann <mail@kevingrossmann.de>
2024-12-30 18:41:11 +0100

    Add Docker Stop Feature - use snake_case for local vars

--- src/backup.sh
+++ src/backup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-lastLogfile="/var/log/backup-last.log"
-lastMailLogfile="/var/log/mail-last.log"
-lastMicrosoftTeamsLogfile="/var/log/microsoft-teams-last.log"
+last_logfile="/var/log/backup-last.log"
+last_mail_logfile="/var/log/mail-last.log"
+last_microsoft_teams_logfile="/var/log/microsoft-teams-last.log"
+
+DOCKER_SOCK="/var/run/docker.sock"
 
 copyErrorLog() {
-  cp ${lastLogfile} /var/log/backup-error-last.log
+  cp ${last_logfile} /var/log/backup-error-last.log
 }
 
 logLast() {
-  echo "$1" >> ${lastLogfile}
+  echo "$1" >> ${last_logfile}
 }
 
 if [ -f "/hooks/pre-backup.sh" ]; then
@@ -19,10 +21,30 @@ else
     echo "Pre-backup script not found ..."
 fi
 
+if [ -S "$DOCKER_SOCK" ]; then
+  temp_file="$(mktemp)"
+  docker ps --format "{{.ID}}" --filter "label=docker-volume-backup.stop-during-backup=true" $CUSTOM_LABEL > "$temp_file"
+  containers_to_stop="$(cat $temp_file | tr '\n' ' ')"
+  containers_to_stop_total="$(cat $temp_file | wc -l)"
+  containers_total="$(docker ps --format "{{.ID}}" | wc -l)"
+  rm "$temp_file"
+  echo "$containers_total containers running on host in total"
+  echo "$containers_to_stop_total containers marked to be stopped during backup"
+else
+  containers_to_stop_total="0"
+  containers_total="0"
+  echo "Cannot access \"$DOCKER_SOCK\", won't look for containers to stop"
+fi
+
+if [ "$containers_to_stop_total" != "0" ]; then
+  info "Stopping containers"
+  docker stop $containers_to_stop
+fi
+
 start=`date +%s`
-rm -f ${lastLogfile} ${lastMailLogfile}
+rm -f ${last_logfile} ${last_mail_logfile}
 echo "Starting Backup at $(date +"%Y-%m-%d %H:%M:%S")"
-echo "Starting Backup at $(date)" >> ${lastLogfile}
+echo "Starting Backup at $(date)" >> ${last_logfile}
 logLast "BACKUP_CRON: ${BACKUP_CRON}"
 logLast "RESTIC_TAG: ${RESTIC_TAG}"
 logLast "RESTIC_FORGET_ARGS: ${RESTIC_FORGET_ARGS}"
@@ -31,20 +53,20 @@ logLast "RESTIC_REPOSITORY: ${RESTIC_REPOSITORY}"
 logLast "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
 
 # Do not save full backup log to logfile but to backup-last.log
-restic backup /data ${RESTIC_JOB_ARGS} --tag=${RESTIC_TAG?"Missing environment variable RESTIC_TAG"} >> ${lastLogfile} 2>&1
-backupRC=$?
+restic backup /data ${RESTIC_JOB_ARGS} --tag=${RESTIC_TAG?"Missing environment variable RESTIC_TAG"} >> ${last_logfile} 2>&1
+backup_rc=$?
 logLast "Finished backup at $(date)"
-if [[ $backupRC == 0 ]]; then
+if [[ $backup_rc == 0 ]]; then
     echo "Backup Successful"
 else
-    echo "Backup Failed with Status ${backupRC}"
+    echo "Backup Failed with Status ${backup_rc}"
     restic unlock
     copyErrorLog
 fi
 
-if [[ $backupRC == 0 ]] && [ -n "${RESTIC_FORGET_ARGS}" ]; then
+if [[ $backup_rc == 0 ]] && [ -n "${RESTIC_FORGET_ARGS}" ]; then
     echo "Forget about old snapshots based on RESTIC_FORGET_ARGS = ${RESTIC_FORGET_ARGS}"
-    restic forget ${RESTIC_FORGET_ARGS} >> ${lastLogfile} 2>&1
+    restic forget ${RESTIC_FORGET_ARGS} >> ${last_logfile} 2>&1
     rc=$?
     logLast "Finished forget at $(date)"
     if [[ $rc == 0 ]]; then
@@ -60,29 +82,34 @@ end=`date +%s`
 echo "Finished Backup at $(date +"%Y-%m-%d %H:%M:%S") after $((end-start)) seconds"
 
 if [ -n "${TEAMS_WEBHOOK_URL}" ]; then
-    teamsTitle="Restic Last Backup Log"
-    teamsMessage=$( cat ${lastLogfile} | sed 's/"/\"/g' | sed "s/'/\'/g" | sed ':a;N;$!ba;s/\n/\n\n/g' )
-    teamsReqBody="{\"title\": \"${teamsTitle}\", \"text\": \"${teamsMessage}\" }"
-    sh -c "curl -H 'Content-Type: application/json' -d '${teamsReqBody}' '${TEAMS_WEBHOOK_URL}' > ${lastMicrosoftTeamsLogfile} 2>&1"
+    teams_title="Restic Last Backup Log"
+    teams_message=$( cat ${last_logfile} | sed 's/"/\"/g' | sed "s/'/\'/g" | sed ':a;N;$!ba;s/\n/\n\n/g' )
+    teams_req_body="{\"title\": \"${teams_title}\", \"text\": \"${teams_message}\" }"
+    sh -c "curl -H 'Content-Type: application/json' -d '${teams_req_body}' '${TEAMS_WEBHOOK_URL}' > ${last_microsoft_teams_logfile} 2>&1"
     if [ $? == 0 ]; then
         echo "Microsoft Teams notification successfully sent."
     else
-        echo "Sending Microsoft Teams notification FAILED. Check ${lastMicrosoftTeamsLogfile} for further information."
+        echo "Sending Microsoft Teams notification FAILED. Check ${last_microsoft_teams_logfile} for further information."
     fi
 fi
 
 if [ -n "${MAILX_ARGS}" ]; then
-    sh -c "mail -v -S sendwait ${MAILX_ARGS} < ${lastLogfile} > ${lastMailLogfile} 2>&1"
+    sh -c "mail -v -S sendwait ${MAILX_ARGS} < ${last_logfile} > ${last_mail_logfile} 2>&1"
     if [ $? == 0 ]; then
         echo "Mail notification successfully sent."
     else
-        echo "Sending mail notification FAILED. Check ${lastMailLogfile} for further information."
+        echo "Sending mail notification FAILED. Check ${last_mail_logfile} for further information."
     fi
+fi
+
+if [ "$containers_to_stop_total" != "0" ]; then
+  info "Starting containers back up"
+  docker start $containers_to_stop
 fi
 
 if [ -f "/hooks/post-backup.sh" ]; then
     echo "Starting post-backup script ..."
-    /hooks/post-backup.sh $backupRC
+    /hooks/post-backup.sh $backup_rc
 else
     echo "Post-backup script not found ..."
 fi
--- src/check.sh
+++ src/check.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-lastLogfile="/var/log/check-last.log"
-lastMailLogfile="/var/log/check-mail-last.log"
-lastMicrosoftTeamsLogfile="/var/log/check-microsoft-teams-last.log"
+last_logfile="/var/log/check-last.log"
+last_mail_logfile="/var/log/check-mail-last.log"
+last_microsoft_teams_logfile="/var/log/check-microsoft-teams-last.log"
 
 copyErrorLog() {
-  cp ${lastLogfile} /var/log/check-error-last.log
+  cp ${last_logfile} /var/log/check-error-last.log
 }
 
 logLast() {
-  echo "$1" >> ${lastLogfile}
+  echo "$1" >> ${last_logfile}
 }
 
 if [ -f "/hooks/pre-check.sh" ]; then
@@ -20,9 +20,9 @@ else
 fi
 
 start=`date +%s`
-rm -f ${lastLogfile} ${lastMailLogfile}
+rm -f ${last_logfile} ${last_mail_logfile}
 echo "Starting Check at $(date +"%Y-%m-%d %H:%M:%S")"
-echo "Starting Check at $(date)" >> ${lastLogfile}
+echo "Starting Check at $(date)" >> ${last_logfile}
 logLast "CHECK_CRON: ${CHECK_CRON}"
 logLast "RESTIC_DATA_SUBSET: ${RESTIC_DATA_SUBSET}"
 logLast "RESTIC_REPOSITORY: ${RESTIC_REPOSITORY}"
@@ -30,16 +30,16 @@ logLast "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
 
 # Do not save full check log to logfile but to check-last.log
 if [ -n "${RESTIC_DATA_SUBSET}" ]; then
-    restic check --read-data-subset=${RESTIC_DATA_SUBSET} >> ${lastLogfile} 2>&1
+    restic check --read-data-subset=${RESTIC_DATA_SUBSET} >> ${last_logfile} 2>&1
 else
-    restic check >> ${lastLogfile} 2>&1
+    restic check >> ${last_logfile} 2>&1
 fi
-checkRC=$?
+check_rc=$?
 logLast "Finished check at $(date)"
-if [[ $checkRC == 0 ]]; then
+if [[ $check_rc == 0 ]]; then
     echo "Check Successful"
 else
-    echo "Check Failed with Status ${checkRC}"
+    echo "Check Failed with Status ${check_rc}"
     restic unlock
     copyErrorLog
 fi
@@ -48,29 +48,29 @@ end=`date +%s`
 echo "Finished Check at $(date +"%Y-%m-%d %H:%M:%S") after $((end-start)) seconds"
 
 if [ -n "${TEAMS_WEBHOOK_URL}" ]; then
-    teamsTitle="Restic Last Check Log"
-    teamsMessage=$( cat ${lastLogfile} | sed 's/"/\"/g' | sed "s/'/\'/g" | sed ':a;N;$!ba;s/\n/\n\n/g' )
-    teamsReqBody="{\"title\": \"${teamsTitle}\", \"text\": \"${teamsMessage}\" }"
-    sh -c "curl -H 'Content-Type: application/json' -d '${teamsReqBody}' '${TEAMS_WEBHOOK_URL}' > ${lastMicrosoftTeamsLogfile} 2>&1"
+    teams_title="Restic Last Check Log"
+    teams_message=$( cat ${last_logfile} | sed 's/"/\"/g' | sed "s/'/\'/g" | sed ':a;N;$!ba;s/\n/\n\n/g' )
+    teams_req_body="{\"title\": \"${teams_title}\", \"text\": \"${teams_message}\" }"
+    sh -c "curl -H 'Content-Type: application/json' -d '${teams_req_body}' '${TEAMS_WEBHOOK_URL}' > ${last_microsoft_teams_logfile} 2>&1"
     if [ $? == 0 ]; then
         echo "Microsoft Teams notification successfully sent."
     else
-        echo "Sending Microsoft Teams notification FAILED. Check ${lastMicrosoftTeamsLogfile} for further information."
+        echo "Sending Microsoft Teams notification FAILED. Check ${last_microsoft_teams_logfile} for further information."
     fi
 fi
 
 if [ -n "${MAILX_ARGS}" ]; then
-    sh -c "mail -v -S sendwait ${MAILX_ARGS} < ${lastLogfile} > ${lastMailLogfile} 2>&1"
+    sh -c "mail -v -S sendwait ${MAILX_ARGS} < ${last_logfile} > ${last_mail_logfile} 2>&1"
     if [ $? == 0 ]; then
         echo "Mail notification successfully sent."
     else
-        echo "Sending mail notification FAILED. Check ${lastMailLogfile} for further information."
+        echo "Sending mail notification FAILED. Check ${last_mail_logfile} for further information."
     fi
 fi
 
 if [ -f "/hooks/post-check.sh" ]; then
     echo "Starting post-check script ..."
-    /hooks/post-check.sh $checkRC
+    /hooks/post-check.sh $check_rc
 else
     echo "Post-check script not found ..."
 fi
